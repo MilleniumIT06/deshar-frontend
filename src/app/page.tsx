cc96d595a346358060e4736d064455874de4f673
Islam Parchiev <islamparchev006@gmail.com>
2025-04-24 09:48:04 +0300

    add: Info section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/Hero";
 import styles from "./page.module.scss";
+import { Info } from "@/components/Info";
 
 export default function Home() {
 
   return (
     <main className={styles.main}>
-      <Hero/>
+      <Hero />
+      <Info />
     </main>
   );
 }
---
+++ src/components/Info/index.tsx
@@ -0,0 +1,28 @@
+import styles from './styles.module.scss';
+import cn from 'classnames';
+export const Info = () => {
+    return (
+        <section className={styles.index}>
+            <h2 className="is-hidden">info section</h2>
+            <div className="container">
+                <div className={styles.inner}>
+                    <ul className={cn("list-reset", styles.list)}>
+                        <li className={styles.item}>
+                            <span>1268</span>
+                            <p>обучающих уроков</p>
+                        </li>
+                        <li className={styles.item}>
+                            <span>100+</span>
+                            <p>квалифицированных учителей</p>
+                        </li>
+                        <li className={styles.item}>
+                            <span>86%</span>
+                            <p>учеников, прошедших аттестацию</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+    )
+}
