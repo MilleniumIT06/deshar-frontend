a16439ef5fff953068863dad05c290cf0e2ee88d
Islam Parchiev <IslamParchievv@yandex.ru>
2025-06-23 16:44:36 +0300

    fix: accessability

--- src/shared/ui/Tabs/index.tsx
+++ src/shared/ui/Tabs/index.tsx
@@ -6,18 +6,31 @@ interface TabsProps {
     tabs: { id: number; name: string; }[];
     activeTab: number;
     handleTab: (id: number) => void;
+    maxWidth?: boolean;
 }
 export const Tabs = ({
     tabs,
     activeTab,
-    handleTab
+    handleTab,
+    maxWidth
 }: TabsProps) => {
     return (
-        <div className={styles.tabs}>
+        <div
+            className={cn(styles.tabs, maxWidth && styles.maxWidth)}
+            role="tablist"
+            aria-label="Навигация по разделам">
             {tabs.map((tab) => (
-                <div key={tab.id} className={cn(styles.tab, tab.id === activeTab && styles.active)} onClick={() => handleTab(tab.id)}>
+                <button
+                    key={tab.id}
+                    className={cn(styles.tab, tab.id === activeTab && styles.active)}
+                    onClick={() => handleTab(tab.id)}
+                    role="tab"
+                    aria-selected={tab.id === activeTab}
+                    aria-controls={`tabpanel-${tab.id}`}
+                    tabIndex={tab.id === activeTab ? -1 : 0}>
+
                     <span>{tab.name}</span>
-                </div>
+                </button>
             ))}
         </div >
     )
