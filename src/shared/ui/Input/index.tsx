091b4b2622c70fec12b7362b09e131262852e17b
Islam Parchiev <IslamParchievv@yandex.ru>
2025-06-23 11:53:26 +0300

    fix: Input

--- src/shared/ui/Input/index.tsx
+++ src/shared/ui/Input/index.tsx
@@ -3,7 +3,6 @@ import { useState, useCallback } from 'react';
 import cn from 'classnames';
 import styles from './styles.module.scss';
 import { cva, type VariantProps } from 'class-variance-authority';
-import { EyeIcon, EyeOffIcon } from 'lucide-react';
 
 const inputVariants = cva(styles.base, {
     variants: {
@@ -84,7 +83,16 @@ const Input = ({
                         aria-label={showPassword ? "Hide password" : "Show password"}
                     // tabIndex={-1} // Предотвращает фокусировку при табинге
                     >
-                        {showPassword ? <EyeIcon size={18} /> : <EyeOffIcon size={18} />}
+                        {showPassword ? <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M22 12C20 16 16.4444 19 12 19C7.55556 19 4 16 2 12C4 8 7.55556 5 12 5C16.4444 5 20 8 22 12Z" stroke-width="2" />
+                            <circle cx="12" cy="12" r="3" stroke-width="2" />
+                        </svg>
+                            :
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.5 17.2931C15.8976 18.3663 14.0427 19 12 19C7.55556 19 4 16 2 12C3.10926 9.78147 4.69704 7.87056 6.6496 6.60849M9.5 5.3291C10.296 5.11468 11.131 5 12 5C16.4444 5 20 8 22 12C21.3987 13.2026 20.6568 14.3148 19.7924 15.2823" stroke-width="2" />
+                                <path d="M14.1213 14.1213C12.9497 15.2929 11.0502 15.2929 9.87866 14.1213C8.70709 12.9497 8.70709 11.0502 9.87866 9.87866" stroke-width="2" />
+                                <path d="M4 4L20 20" stroke-width="2" stroke-linecap="round" />
+                            </svg>}
                     </button>
                 )
             }
