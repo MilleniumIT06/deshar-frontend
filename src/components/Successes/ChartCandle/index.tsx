f6802e0779f34f15c2998d1a65e6975c0260fa70
Islam Parchiev <IslamParchievv@yandex.ru>
2025-08-06 16:15:07 +0300

    refactor(ChartCandle):Replace div into svg

--- src/components/Successes/ChartCandle/index.tsx
+++ src/components/Successes/ChartCandle/index.tsx
@@ -1,17 +1,17 @@
 import styles from './styles.module.scss';
 
 export interface ChartCandleProps {
-    id:number|string;
+    id: number | string;
     maxPoints: number;
     currentPoints: number;
-    date?: Date; 
+    date?: Date;
 }
 
 export const ChartCandle = ({
     id,
     maxPoints,
     currentPoints,
-    date = new Date() 
+    date = new Date()
 }: ChartCandleProps) => {
 
     const safeMaxPoints = Math.max(1, maxPoints);
@@ -21,15 +21,15 @@ export const ChartCandle = ({
         (clampedPoints / safeMaxPoints) * 100
     ));
 
-    const formatDate = (date: Date):{day:number;month:string;} => {
+    const formatDate = (date: Date): { day: number; month: string; } => {
         const day = date.getDate();
         const month = date.toLocaleString('ru-RU', { month: "short" });
-        return {day:day, month:month.slice(0,3)}
+        return { day: day, month: month.slice(0, 3) }
     };
     const dateT = formatDate(date);
     return (
         <div className={styles.index}>
-            <div
+            {/* <div
                 className={styles.index__body}
                 style={{
                     height: `${pointsPercent}%`,
@@ -38,7 +38,20 @@ export const ChartCandle = ({
                 aria-valuenow={clampedPoints}
                 aria-valuemin={0}
                 aria-valuemax={safeMaxPoints}
-            />
+            /> */}
+            <svg
+                className={styles.index__body}
+                width="45"
+                height={`${pointsPercent}%`}
+                viewBox="0 0 45 174"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                role="progressbar"
+                aria-valuenow={clampedPoints}
+                aria-valuemin={0}
+                aria-valuemax={safeMaxPoints}>
+                <rect x="0.5" width="44" height="100%" rx="12" fill="#1BAA7D" />
+            </svg>
             <time className={styles.index__date}>
                 <span>{dateT.day}</span>
                 <span>{dateT.month}</span>
