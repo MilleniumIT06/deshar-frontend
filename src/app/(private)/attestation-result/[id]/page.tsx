a02ef5537803504fe4c5d0e333363a43318b9707
Islam Parchiev <IslamParchievv@yandex.ru>
2025-09-09 16:59:29 +0300

    fix:remove css import

--- src/app/(private)/attestation-result/[id]/page.tsx
+++ src/app/(private)/attestation-result/[id]/page.tsx
@@ -1,10 +1,8 @@
 import { AttestationResult } from '@/components/AttestationResult'
 
-import './../styles.scss'
-
 export default async function AttestationResultPage() {
 	return (
-		<main className="main">
+		<main>
 			<AttestationResult />
 		</main>
 	)
