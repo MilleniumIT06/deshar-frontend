b4af5bca773ba56ad77fcdbb561c75abfed6fde7
Islam Parchiev <IslamParchievv@yandex.ru>
2025-09-12 11:40:04 +0300

    response(header):add burger, remove name in Avatar

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -11,11 +11,12 @@ import './styles.scss'
 
 export const Header = () => {
 	const [authed] = useState(true)
+	const [burgerOpen, setBurgerOpen] = useState(false)
 	return (
 		<header className="Header">
 			<div className="container Header__container">
 				<div className="Header__inner">
-					<Logo size="large" />
+					<Logo size="large" className="Header__logo" />
 					<nav className="Header__nav">
 						<ul className={`list-reset Header__list`}>
 							<li className="Header__list_item">
@@ -35,20 +36,25 @@ export const Header = () => {
 							</li>
 						</ul>
 					</nav>
-					{authed ? (
-						<Avatar />
-					) : (
-						<Button variant="primary" size="small" className="Header__btn" tabIndex={6}>
-							Войти
-						</Button>
-					)}
-					<button
-						className="btn-reset Header__burger"
-						aria-label="Открыть меню"
-						aria-expanded="false"
-						data-burger>
-						<span className="Header__line" />
-					</button>
+					<div className="Header__right">
+						{authed ? (
+							<Avatar />
+						) : (
+							<Button variant="primary" size="small" className="Header__btn" tabIndex={6}>
+								Войти
+							</Button>
+						)}
+						<button
+							className={`btn-reset Header__burger ${burgerOpen ? 'active' : ''}`}
+							aria-label="Открыть меню"
+							aria-expanded="false"
+							data-burger
+							onClick={() => setBurgerOpen(prev => !prev)}>
+							<span className="Header__burger_line" />
+							<span className="Header__burger_line" />
+							<span className="Header__burger_line" />
+						</button>
+					</div>
 				</div>
 			</div>
 		</header>
