06499ad9d1ee8d703a8446ae8c26ca36d5c8eba1
Islam Parchiev <IslamParchievv@yandex.ru>
2025-09-25 12:48:58 +0300

    fixes

--- src/features/auth/SignIn/ui/SignInForm/index.tsx
+++ src/features/auth/SignIn/ui/SignInForm/index.tsx
@@ -1,23 +1,56 @@
+'use client'
 import Link from 'next/link'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
 import { Button } from '@/shared/ui/Button'
 import { Input } from '@/shared/ui/Input'
 
 import './styles.scss'
+import { signInUserFormSchema, type signInUserFormData } from '../../model/signIn.schema'
 
 export const SignInForm = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<signInUserFormData>({
+		resolver: zodResolver(signInUserFormSchema),
+		mode: 'onChange',
+	})
+	const onSubmit = (data: signInUserFormData) => {
+		// eslint-disable-next-line no-console
+		console.log(data)
+	}
 	return (
 		<div className="SignInForm">
 			<div className="SignInForm__inner">
 				<h1 className="SignInForm__title">Вход в систему</h1>
-				<form className="SignInForm__form">
+				<form className="SignInForm__form" onSubmit={handleSubmit(onSubmit)}>
 					<Input
+						fullWidth
+						type="email"
+						placeholder="Введите email"
+						className="SignInForm__input"
+						validationMessage={errors.email && errors.email.message}
+						{...register('email')}
+					/>
+					<Input
+						fullWidth
+						type="password"
+						placeholder="Придумайте пароль"
+						className="SignInForm__input"
+						validationMessage={errors.password && errors.password.message}
+						{...register('password')}
+					/>
+					{/* <Input
 						fullWidth
 						type="email"
 						placeholder="Введите логин или email"
 						className="SignInForm__input"
 					/>
-					<Input fullWidth type="password" placeholder="Введите пароль" className="SignInForm__input" />
+					<Input fullWidth type="password" placeholder="Введите пароль" className="SignInForm__input" /> */}
 					<Button className="SignInForm__btn" size="medium">
 						Войти
 					</Button>
