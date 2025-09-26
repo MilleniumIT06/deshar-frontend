62323e730e4fdb3b6e6ebd4a41cb4ce4d44e25a7
Islam Parchiev <IslamParchievv@yandex.ru>
2025-09-26 10:42:10 +0300

    optimize:add dynamic imports

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,8 +1,14 @@
-import { Continue } from '@/components/Continue'
-import { OtherSubjects } from '@/components/OtherSubjects'
-import { Successes } from '@/components/Successes'
-import { TopClassmates } from '@/components/TopClassmates'
+import dynamic from 'next/dynamic'
 
+import { Continue } from '@/components/Continue'
+import { Successes } from '@/components/Successes'
+
+const OtherSubjects = dynamic(() => import('@/components/OtherSubjects').then(mod => mod.OtherSubjects), {
+	loading: () => <div>Загрузка...</div>,
+})
+const TopClassmates = dynamic(() => import('@/components/TopClassmates').then(mod => mod.TopClassmates), {
+	loading: () => <div>Загрузка...</div>,
+})
 export default function Authed() {
 	return (
 		<main className="mRelative">
