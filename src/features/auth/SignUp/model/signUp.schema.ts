2b31e67882137e1e49b5b750d37446150df8e914
Islam Parchiev <IslamParchievv@yandex.ru>
2025-12-05 09:45:25 +0300

    fix: add max-height and scroll in inputSelect

--- src/features/auth/SignUp/model/signUp.schema.ts
+++ src/features/auth/SignUp/model/signUp.schema.ts
@@ -5,11 +5,10 @@ export const signUpUserSchema = z
 		name: z.string().min(3, 'Введите имя').max(20),
 		surname: z.string().min(3, 'Введите фамилию').max(20),
 		email: z.string().email('Некорректный email'),
-		password: z
-			.string()
-			.min(8, 'Пароль должен быть не менее 8 символов')
-			.regex(/[A-Z]/, 'Пароль должен содержать хотя бы одну заглавную букву')
-			.regex(/[0-9]/, 'Пароль должен содержать хотя бы одну цифру'),
+		password: z.string({ message: 'Пароль обязателен для заполнения' }),
+		// .min(8, 'Пароль должен быть не менее 8 символов')
+		// .regex(/[A-Z]/, 'Пароль должен содержать хотя бы одну заглавную букву')
+		// .regex(/[0-9]/, 'Пароль должен содержать хотя бы одну цифру'),
 		confirmPassword: z.string(),
 	})
 	.refine(data => data.password === data.confirmPassword, {
