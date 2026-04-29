8b0cf46862212b1090cfd3a0167003e123756ecc
Islam Parchiev <IslamParchievv@yandex.ru>
2026-04-29 11:57:26 +0300

    fix(Selector): add lazy motion for animations

--- src/app/_providers/providers.tsx
+++ src/app/_providers/providers.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Provider } from 'react-redux'
 
 import { store } from '../_store'
+import { domAnimation, LazyMotion } from 'motion/react'
 
 export const Providers = ({ children }: { children: ReactNode }) => {
 	const [client] = useState(
@@ -19,7 +20,9 @@ export const Providers = ({ children }: { children: ReactNode }) => {
 	)
 	return (
 		<QueryClientProvider client={client}>
-			<Provider store={store}>{children}</Provider>
+			<Provider store={store}>
+				<LazyMotion features={domAnimation}>{children}</LazyMotion>
+			</Provider>
 		</QueryClientProvider>
 	)
 }
